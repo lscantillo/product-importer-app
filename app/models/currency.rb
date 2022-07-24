5e96348d2d07c3d532a19abbda7cc6e0ca2374f9
lscantillo <luissebastiancantillo@gmail.com>
2022-07-24 17:09:45 -0500

    WIP: Add Currency model validations

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,7 @@
 class Currency < ApplicationRecord
   has_many :products
+  validates :name, presence: true
+  validates :currency_code, presence: true
+  validates :name, uniqueness: true
+  validates :currency_code, uniqueness: true
 end
