b5e7f5eefa9e2ecc2506a118a7b1f532fa4c016f
lscantillo <luissebastiancantillo@gmail.com>
2022-07-26 20:10:21 -0500

    WIP: Product model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :currency
+  validates :name, :price, :expiration, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :name, uniqueness: true
+  validates :name, format: { with: /.*#\(\d{16}\)/ }
 end
